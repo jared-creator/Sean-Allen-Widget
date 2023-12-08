2c26857b8da8b0bb98f9264bfe604420e9d47815
jared <jaredmurray43@icloud.com>
2023-12-08 15:39:03 -0700

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Jared Murray on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
